25690e6531282928dbc8641a14eb0bb01d51f16c
unknown <ahmedtheconqueror@gmail.com>
2024-08-18 12:14:49 -0700

    adjust gpa

--- src/containers/resume/utils.js
+++ src/containers/resume/utils.js
@@ -21,7 +21,7 @@ export const data = {
     // ],
     education: [
         {
-            title: "B.S., Computer Engineering - GPA: 3.83",
+            title: "B.S., Computer Engineering - GPA: 3.82",
             subTitle: "San Jose State University",
             description: "As a third-year Computer Engineering major, I am deeply passionate about crafting innovative software solutions to address contemporary challenges.",
 
