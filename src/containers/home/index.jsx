bebed768a72aa0c2db75698a516e87936c00e547
unknown <ahmedtheconqueror@gmail.com>
2024-09-15 17:06:23 -0700

    add leetcode page

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
 import { Animate } from 'react-simple-animate';
-import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaCode } from 'react-icons/fa';
 import './styles.scss';
 import ImageSeven from '../../images/image7.png'
 
